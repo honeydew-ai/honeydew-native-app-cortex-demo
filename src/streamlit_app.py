0ea07c5839a004d3b9cb4cde018dae830bdcd539
Baruch Oxman <baruch@honeydew.ai>
2024-06-24 13:32:38 +0300

    Update the streamlit app with latest changes

--- src/streamlit_app.py
+++ src/streamlit_app.py
@@ -1,26 +1,36 @@
 import streamlit as st
+import pandas as pd
 import json
 import re
+import datetime
+
 from snowflake.snowpark.context import get_active_session
 
 ##### Constants
 
 ## Name of installed Native Application
-HONEYEW_APP = "HONEYDEW_APP"
+HONEYDEW_APP = "HONEYDEW_APP"
 
 ## Honeydew workspace and branch
 WORKSPACE = "tpch_demo"
 BRANCH = "prod"
 
-## Mark in Schema with a label (like 'ai-ready') to filter for part of the schema
-AI_READY_LABEL = None
+## Add it to a domain to set what is exposed to LLMs
+AI_DOMAIN = "llm"
 
 ## Set to '1' to see sent queries
 DEBUG = 0
 
+## Set to True if schema contains parameters
+SUPPORT_PARAMETERS = False
+
+TODAY = datetime.datetime.now().strftime("%Y-%m-%d")
+
 ## Cortex Prompt Template
 PROMPT = """
 Act as the Honeydew assistant to help users see data. Follow the role described below. 
+
+The date today is {today} 
 
 # Your Task
 
@@ -64,8 +74,12 @@ A response will be a list of metrics relevant to customers in the schema
 
 ## Dealing with dates
 
-Use date entity for date comparisons. 
-For example, last_month is "date.month >= CURRENT_DATE() - INTERVAL ''''1 month''''".
+Use date entity for date comparisons.  
+
+Use following Snowflake SQL functions for dates:
+* `CURRENT_DATE` to refer to now
+* `DATE_TRUNC` to get boundaries of a time window, i.e. `DATE_TRUNC(month, CURRENT_DATE())` for month
+* `INTEVAL` to see relative time, i.e last_month is "date.month >= CURRENT_DATE() - INTERVAL ''''1 month''''".
 
 # Your Rules
 
@@ -111,37 +125,56 @@ User input follows:
 
 session = get_active_session()
 
+
 ## Load Schema from Honeydew
 @st.cache_data
 def get_schema():
     st.write("Loading Schema")
-    data = session.sql(f"select * from table({HONEYEW_APP}.API.SHOW_FIELDS('{WORKSPACE}','{BRANCH}'))" + (f" where array_contains('{AI_READY_LABEL}'::variant, labels)" if AI_READY_LABEL is not None else ""))
+    ## Load the AI domain only
+    data = session.sql(
+        f"""
+with 
+ entities as (select entities from table({HONEYDEW_APP}.API.SHOW_DOMAINS('{WORKSPACE}','{BRANCH}')) where name='{AI_DOMAIN}'),
+ entities_unnest as (select f.value:name::varchar as name, f.value:fields::array(varchar) as fields from entities e, LATERAL FLATTEN(input => e.entities) f),
+ domain_fields as (select entity, fields.name, type, datatype, description from table(HONEYDEW_APP.API.SHOW_FIELDS('{WORKSPACE}','{BRANCH}')) fields 
+   JOIN  entities_unnest 
+   ON fields.entity = entities_unnest.name and (entities_unnest.fields is null or array_contains(fields.name, entities_unnest.fields)))
+select * from domain_fields
+"""
+    )
     return data.to_pandas()
+
 
 ## Load Parameter default values from Honeydew
 @st.cache_data
 def get_parameters():
-    data = session.sql(f"select * from table({HONEYEW_APP}.API.SHOW_GLOBAL_PARAMETERS('{WORKSPACE}','{BRANCH}'))")
-    return data.to_pandas().to_dict('records') 
+    data = session.sql(
+        f"select * from table({HONEYDEW_APP}.API.SHOW_GLOBAL_PARAMETERS('{WORKSPACE}','{BRANCH}'))"
+    )
+    return data.to_pandas().to_dict("records")
+
 
 @st.cache_data
 def convert_df(df):
-    return df.to_csv().encode('utf-8')
+    return df.to_csv().encode("utf-8")
+
 
 ## Helper function for prompt building
 def make_list_from_schema(schema):
     return "\n".join(
-         f"{val['ENTITY']}.{val['NAME']} ({val['DATATYPE']})" + 
-        (f" - {val['DESCRIPTION']}" if val['DESCRIPTION'] is not None else "")
-        for val in schema.to_dict('records') 
+        f"{val['ENTITY']}.{val['NAME']} ({val['DATATYPE']})"
+        + (f" - {val['DESCRIPTION']}" if val["DESCRIPTION"] is not None else "")
+        for val in schema.to_dict("records")
     )
+
 
 ## Build prompt from template + schema
 def build_sys_prompt():
     schema = get_schema()
-    attrs = make_list_from_schema(schema.loc[schema['TYPE'] != 'Metric'])
-    metrics = make_list_from_schema(schema.loc[schema['TYPE'] == 'Metric'])
-    return PROMPT.format(attributes=attrs, metrics=metrics)
+    attrs = make_list_from_schema(schema.loc[schema["TYPE"] != "Metric"])
+    metrics = make_list_from_schema(schema.loc[schema["TYPE"] == "Metric"])
+    return PROMPT.format(attributes=attrs, metrics=metrics, today=TODAY)
+
 
 ## Cortex LLM wrapper functions
 def get_single_sql_with_debug(sql):
@@ -155,72 +188,120 @@ def escape_quote(str):
     str = re.sub(r"'+", "''", str, flags=re.S)
     return str
 
-def run_cortex(sys_prompt, user_question, model='mistral-large', temperature=0.2):
+
+def run_cortex(sys_prompt, user_question, model="mistral-large", temperature=0.2):
     with st.spinner("Asking AI..."):
-        data = get_single_sql_with_debug(f"""
+        data = get_single_sql_with_debug(
+            f"""
         SELECT SNOWFLAKE.CORTEX.COMPLETE('{model}', 
         [
             {{'role':'system', 'content': '{escape_quote(sys_prompt)}'}},
             {{'role':'user', 'content': '{escape_quote(user_question)}'}}
         ], 
-        {{'temperature': {temperature}, 'max_tokens': 500}}) """)
+        {{'temperature': {temperature}, 'max_tokens': 500}}) """
+        )
         return json.loads(data)
 
+
 ## Explanation widget helper functions
+
 
 def format_as_list(response, key):
     if key not in response:
         return "[]"
     return "[" + ", ".join(f"'{escape_quote(val)}'" for val in response[key]) + "]"
 
+
 def write_explain(response):
     s = ""
-    if response.get('group_by'):
+    if response.get("group_by"):
         s += """
 * Attributes
 """ + "\n".join(
-            f"  * [{val}](https://app.honeydew.cloud/workspace/{WORKSPACE}/version/{BRANCH}/entity/{val.split('.')[0]}/attribute/{val.split('.')[1]})" for val in response['group_by'])
-    if response.get('metrics'):
+            f"  * [{val}](https://app.honeydew.cloud/workspace/{WORKSPACE}/version/{BRANCH}/entity/{val.split('.')[0]}/attribute/{val.split('.')[1]})"
+            for val in response["group_by"]
+        )
+    if response.get("metrics"):
         s += """
 * Metrics
 """ + "\n".join(
-        f"  * [{val}](https://app.honeydew.cloud/workspace/{WORKSPACE}/version/{BRANCH}/entity/{val.split('.')[0]}/metric/{val.split('.')[1]})" for val in response['metrics'])
-    if response.get('filters'):
+            f"  * [{val}](https://app.honeydew.cloud/workspace/{WORKSPACE}/version/{BRANCH}/entity/{val.split('.')[0]}/metric/{val.split('.')[1]})"
+            for val in response["metrics"]
+        )
+    if response.get("filters"):
         s += """
 * Filters
 """ + "\n".join(
-        f"  * `{val}`" for val in response['filters'])
-    
+            f"  * `{val}`" for val in response["filters"]
+        )
+
     st.markdown(s)
+
+
+def make_chart(df):
+    # Bug in streamlit with dots in column names - update column names
+    updated_columns = {col: col.replace(".", "_") for col in df.columns}
+    df = df.rename(columns=updated_columns)
+    numeric_columns = list(df.select_dtypes(include=["number"]).columns)
+    if len(numeric_columns) == 0:
+        return
+    # Bug in snowflake connector - does not set date types correctly in pandas, so manually detecting
+    date_columns = ["date_date", "date_week", "date_month", "date_year", "date_quarter"]
+    # Filter columns that match the specific names and exist in the DataFrame
+    date_columns = list(df.columns.intersection(date_columns))
+    str_columns = df.select_dtypes(include=["object"]).columns
+    str_columns = [col for col in str_columns if col not in date_columns]
+    df_to_show = df[numeric_columns + str_columns]
+    for col in date_columns[:1]:
+        df_to_show[col] = pd.to_datetime(df[col], errors="coerce")
+    if len(date_columns) > 0:
+        df_to_show = df_to_show.rename(columns={date_columns[0]: "index"}).set_index(
+            "index"
+        )
+        if len(str_columns) == 0:
+            st.line_chart(df_to_show)
+        elif len(str_columns) == 1 and len(numeric_columns) == 1:
+            st.line_chart(df_to_show, y=numeric_columns[0], color=str_columns[0])
+    elif len(str_columns) > 0:
+        df_to_show = df_to_show.rename(columns={str_columns[0]: "index"}).set_index(
+            "index"
+        )
+        if len(numeric_columns) == 1:
+            st.bar_chart(df_to_show)
+    elif len(numeric_columns) > 0:
+        st.bar_chart(df_to_show.transpose())
 
 
 ## Functions to process the LLM response
 def replace_parameters(sql):
+    if not SUPPORT_PARAMETERS:
+        return sql
     params = get_parameters()
     for param in params:
         param_name = param["NAME"]
-        param_val = param ["VALUE"]
-        sql = sql.replace(f'${param_name}', param_val)
+        param_val = param["VALUE"]
+        sql = sql.replace(f"${param_name}", param_val)
     return sql
+
 
 def process_response(response):
     if DEBUG:
         st.code(response)
-        
+
     ### Parse LLM response to look for Honeydew queries
-    for m in re.findall('CALL HONEYDEW\(({.*})\)', response, flags=re.S):
+    for m in re.findall("CALL HONEYDEW\(({.*})\)", response, flags=re.S):
         try:
-            # LLM sometimes forgets commas, help it... 
-            cleaned_bad_commas = re.sub(r"]\s*,\s*}","]}", m, re.S)
+            # LLM sometimes forgets commas, help it...
+            cleaned_bad_commas = re.sub(r"]\s*,\s*}", "]}", m, re.S)
             resp_val = json.loads(cleaned_bad_commas)
         except Exception as e:
             # If LLM made bad JSON - ignore and continue
             st.exception(f"Bad JSON: {response}")
             continue
 
-        # Use Semantic Layer to get SQL for the data 
+        # Use Semantic Layer to get SQL for the data
         native_app_sql = f"""
-                select {HONEYEW_APP}.API.GET_SQL_FOR_FIELDS(
+                select {HONEYDEW_APP}.API.GET_SQL_FOR_FIELDS(
                 '{WORKSPACE}','{BRANCH}',
                 NULL,
                 {format_as_list(resp_val, 'group_by')},
@@ -229,34 +310,38 @@ def process_response(response):
             )"""
         try:
             sql = get_single_sql_with_debug(native_app_sql)
-            
+
             # Parameterized SQLs replace parameters with default values
             # due to lack of support for session variables in Streamlit
             sql = replace_parameters(sql)
-            
+
             # Automatically order by date, if date is part of the response
             if "group_by" in resp_val:
-                dates = ",".join(f'"{val}"' for val in resp_val['group_by'] if val.startswith('date.'))
+                dates = ",".join(
+                    f'"{val}"'
+                    for val in resp_val["group_by"]
+                    if val.startswith("date.")
+                )
                 if dates:
                     sql += f"\nORDER BY {dates}"
 
             # Too many rows are not needed
             sql += "\nLIMIT 500"
-            
+
         except Exception as e:
             # If bad semantic query (for example LLM hallucinated a field) - ignore and continue
             st.error(f"Can't get SQL: {e}\n{native_app_sql}")
             continue
-            
+
         if DEBUG:
             st.code(sql)
         return sql, resp_val
     return None, None
 
 
-
 ########## Main flow
-    
+
+
 user_question = st.chat_input("Ask a question about the data")
 
 if user_question:
@@ -268,7 +353,7 @@ if user_question:
         sys_prompt = build_sys_prompt()
         st.write("Generating Semantic Query")
         response = run_cortex(sys_prompt, user_question)
-        message = response['choices'][0]['messages']
+        message = response["choices"][0]["messages"]
         if DEBUG:
             st.code(message)
         st.write("Compiling SQL")
@@ -284,13 +369,14 @@ if user_question:
     # Show response (data or markdown based on answer)
     st.subheader(user_question, divider=True)
     if df is not None:
+        make_chart(df)
         st.dataframe(df, use_container_width=True)
         csv = convert_df(df)
         st.download_button(
             label="Download data as CSV",
             data=csv,
-            file_name='data.csv',
-            mime='text/csv',
+            file_name="data.csv",
+            mime="text/csv",
         )
         with st.expander("Explain", expanded=True):
             write_explain(resp_val)
@@ -301,4 +387,3 @@ if user_question:
 
 if st.button("Reload Schema"):
     st.cache_data.clear()
-
